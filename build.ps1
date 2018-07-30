3abac35bdd6d6565d07626d1ee3252f7482f1de2
Blair Conrad <blair@blairconrad.com>
2018-07-29 20:45:35 -0400

    Highlight zip task in build output

--- build.ps1
+++ build.ps1
@@ -5,4 +5,6 @@ $extensionName = 'Agfa Jira Chrome Extension'
 $extensionSlug = $extensionName.Replace(' ', '')
 $output = "artifacts\output\$($extensionSlug).zip"
 
-.\BuildTools\7z a "$output" .\Extension
+Write-Output "Zipping extension to $output..."
+.\BuildTools\7z a "$output" .\Extension | ForEach-Object { "  " + $_ }
+Write-Output "Zipped extension to $output."
